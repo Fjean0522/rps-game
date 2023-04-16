2f60e4475db77f5cf116ef0efbb13c04cb710b3c
Fjean <fjean0522@gmail.com>
2023-04-15 21:10:09 -0400

    Add and define player and cpu selection variables

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function computerChoice() {
+function cpuChoice() {
     let choice = ['Rock','Paper','Scissors']
     return choice[Math.floor(Math.random() * choice.length)]
 }
 
+
+const playerSelection = prompt('Select Rock, Paper, or Scissors:');
+const cpuSelection = cpuChoice;
+
+function play(playerSelection, cpuSelection) {
+
+}
